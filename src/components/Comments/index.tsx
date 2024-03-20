8aba9a293c7e7d85f0e75003affac74c53165603
Felipe Lima <felipedesouzamali@gmail.com>
2024-03-20 04:37:59 -0300

    edit comments

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -4,9 +4,15 @@ import comments from "../../helpers/comments.json";
 const Comments = () => {
   return (
     <div className="comments">
-      {comments.data.map(({ user, text, career }) => (
-        <Card user={user} text={text} career={career} />
-      ))}
+      <div className="title-container">
+        <h4>O que os clientes falam...</h4>
+      </div>
+
+      <div className="container-card">
+        {comments.data.map(({ user, text, career }) => (
+          <Card user={user} text={text} career={career} />
+        ))}
+      </div>
     </div>
   );
 };
