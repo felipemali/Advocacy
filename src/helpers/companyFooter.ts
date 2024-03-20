9ce05f261a741bdbd147f8654ac1c2f2d6b3e15e
Felipe Lima <felipedesouzamali@gmail.com>
2024-03-20 16:08:08 -0300

    change footer

--- src/helpers/companyFooter.ts
+++ src/helpers/companyFooter.ts
@@ -2,6 +2,5 @@ export const companyFooter = [
   { name: "sobre nós" },
   { name: "nossos serviços" },
   { name: "política de Privacidade" },
-  { name: "nossos serviços" },
   { name: "Programa de Afiliados" },
 ];
